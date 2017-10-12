1e0c91f4c7c422d603f8897c4fb24e8bd56064d2
ChinhBV <buivanchinh.nd@gmail.com>
2017-10-13 00:15:33 +0700

    admin-dashboard: update style table vip share

--- resources/views/admin/dashboard/_vipshare.blade.php
+++ resources/views/admin/dashboard/_vipshare.blade.php
@@ -13,15 +13,15 @@
             </tr>
             </thead>
             <tbody>
-                <tr v-for="item in items">
+                <tr v-for="item in itemsVipShare">
                     <td>@{{ item.idfb }}</td>
                     <td>@{{ item.name }}</td>
-                    <td>@{{ item.goi }}</td>
-                    <td>@{{ item.soshare }}</td>
-                    <td>@{{ item.time }}</td>
-                    <td>    
-                        <button class="btn btn-primary" @click.prevent="editItem(item)">Edit</button>
-                        <button class="btn btn-danger" @click.prevent="deleteItem(item)">Delete</button>
+                    <td><a class="btn btn-xs btn-primary"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i> @{{ item.goi }} Share</a></td>
+                    <td><a class="btn btn-xs btn-primary"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i> @{{ item.soshare }} Share</a></td>
+                    <td><a class="btn btn-xs btn-success"><i class="fa fa-history" aria-hidden="true"></i> @{{ item.time }} </a></td>
+                    <td>
+                        <a href="#" class="btn btn-xs btn-danger"><i class="fa fa-user-times" aria-hidden="true"></i> Xóa Tài Khoản</a>
+                        <a href="#" class="btn btn-xs btn-danger"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Chỉnh Sửa</a>
                     </td>
                 </tr>
             </tbody>
@@ -29,20 +29,20 @@
         <!-- Pagination -->
         <nav>
             <ul class="pagination">
-                <li v-if="pagination.current_page > 1">
+                <li v-if="paginationVipShare.current_page > 1">
                     <a href="#" aria-label="Previous"
                        @click.prevent="changePageVipShare(pagination.current_page - 1)">
                         <span aria-hidden="true">«</span>
                     </a>
                 </li>
-                <li v-for="page in pagesNumber"
-                    v-bind:class="[ page == isActived ? 'active' : '']">
+                <li v-for="page in pagesNumberVipShare"
+                    v-bind:class="[ page == isActivedVipShare ? 'active' : '']">
                     <a href="#"
-                       @click.prevent="changePage(page)">@{{ page }}</a>
+                       @click.prevent="changePageVipShare(page)">@{{ page }}</a>
                 </li>
-                <li v-if="pagination.current_page < pagination.last_page">
+                <li v-if="paginationVipShare.current_page < paginationVipShare.last_page">
                     <a href="#" aria-label="Next"
-                       @click.prevent="changePage(pagination.current_page + 1)">
+                       @click.prevent="changePageVipShare(paginationVipShare.current_page + 1)">
                         <span aria-hidden="true">»</span>
                     </a>
                 </li>
