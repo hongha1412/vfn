007ea4c9caa05fb1cf07f1749ca6a77e4a1dcdc1
ChinhBV <buivanchinh.nd@gmail.com>
2017-10-16 23:06:21 +0700

    admin: update view _confirm.blade

--- resources/views/admin/layouts/_confirm.blade.php
+++ resources/views/admin/layouts/_confirm.blade.php
@@ -1,26 +1,19 @@
 <div id="confirmDeleteModal" class="modal fade" role="dialog">
     <div class="modal-dialog">
-
         <!-- Modal content-->
         <div class="modal-content">
-            <div class="modal-header">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <b>Xác Nhận Xóa</b>
-                    </div>
-                    <div class="panel-body">
-                        <form method="POST" v-on:submit.prevent="submitDelete()">
-                            <div class="form-group">
-                                <label for="usr" style="color:red">Bạn Có Chắc Chắn Muốn Xóa?</label>
-                            </div>
-                            <button type="submit" name="submitDelete" class="btn btn-danger">Xóa</button>
-                            <button type="button" name="deleteClose" class="btn btn-default" data-dismiss="modal">
-                                Thoát
-                            </button>
-                        </form>
-                    </div>
+            <form method="POST" v-on:submit.prevent="submitDelete()">
+                <div class="modal-header">
+                    <h4 class="modal-title"><b>Xác Nhận Xóa</b></h4>
                 </div>
-            </div>
+                <div class="modal-body">
+                    <label for="usr" style="color:red">Bạn Có Chắc Chắn Muốn Xóa?</label>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" name="submitDelete" class="btn btn-danger">Xóa</button>
+                    <button type="button" name="deleteClose" class="btn btn-default" data-dismiss="modal">Thoát</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
