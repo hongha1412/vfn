c171d8ee0cbd8e808fdf4ffe80ffdcda0a108934
vanchinh1989 <vanchinh1989@gmail.com>
2017-10-20 13:15:42 +0700

    admin/daypackage: add controller

---
+++ app/Http/Controllers/Admin/DayPackageController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\DayPackage;
+
+class DayPackageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $perPage =  isset($_GET["perPage"]) ? $_GET["perPage"] : 10;
+        $accounts = DayPackage::paginate($perPage);
+
+        $response = [
+            'pagination' => [
+                'total'        => $accounts->total(),
+                'per_page'     => $accounts->perPage(),
+                'current_page' => $accounts->currentPage(),
+                'last_page'    => $accounts->lastPage(),
+                'from'         => $accounts->firstItem(),
+                'to'           => $accounts->lastItem()
+            ],
+            'data' => $accounts
+        ];
+
+        return response()->json($response);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'daytotal'    => 'required'
+        ]);
+        
+        $create = new DayPackage;
+        $edit->daytotal = $request->daytotal;
+        $create->save();
+
+        return response()->json($create);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'daytotal'    => 'required'
+        ]);
+        
+        $edit = DayPackage::find($id);
+        $edit->daytotal = $request->daytotal;
+        $edit->save();
+
+        return response()->json($edit);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $vipList = Vip::getVipByPackage($id);
+        if (sizeof($vipList) > 0) {
+            return response()->json(array('error' => true, 'message' => 'Day package đang được sử dụng'));
+        } 
+
+        DayPackage::find($id)->delete();
+        
+        return response()->json(array('error' => false, 'message' => 'Xóa day package thành công'));
+    }
+}
